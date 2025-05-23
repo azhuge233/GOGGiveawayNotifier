ded4fe260ddf9baee0aa42a3a449062828fcdd97
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-05-24 04:26:03 +0800

    Change record model

--- GOGGiveawayNotifier/Model/Record/GiveawayRecord.cs
+++ GOGGiveawayNotifier/Model/Record/GiveawayRecord.cs
@@ -1,6 +1,14 @@
-﻿namespace GOGGiveawayNotifier.Model {
+﻿using System;
+
+namespace GOGGiveawayNotifier.Model {
 	public class GiveawayRecord {
-		public string Name { get; set; }
+		public string Type { get; set; }
+		public string ID { get; set; }
 		public string Url { get; set; }
+		public string Title { get; set; }
+		public string Slug { get; set; }
+		public DateTime? EndDate { get; set; }
+		public string ProductType { get; set; }
+		public string ProductState { get; set; }
 	}
 }
