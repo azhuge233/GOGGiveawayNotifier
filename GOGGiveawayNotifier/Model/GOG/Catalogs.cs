05d99c9ffe911deb1a8bbd294b8d9c487eb4a43d
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-05-24 07:13:42 +0800

    Add GOG catalog model.

--- GOGGiveawayNotifier/Model/GOG/Catalogs.cs
+++ GOGGiveawayNotifier/Model/GOG/Catalogs.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace GOGGiveawayNotifier.Model.GOG {
 	public class Catalogs {
+		[JsonProperty("productCount")]
+		public int ProductCount { get; set; }
+		[JsonProperty("products")]
 		public List<Product> Products { get; set; } = [];
 	}
 }
