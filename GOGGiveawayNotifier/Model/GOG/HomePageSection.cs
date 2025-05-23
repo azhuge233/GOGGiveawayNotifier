69e147db02f8c7e1dbe8ac2e8ec33e80d924d5ab
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-05-24 03:28:43 +0800

    Add HomePageSection Model.

---
+++ GOGGiveawayNotifier/Model/GOG/HomePageSection.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace GOGGiveawayNotifier.Model.GOG {
+	public class HomePageSection {
+		[JsonProperty("sectionId")]
+		public string SectionID { get; set; }
+		[JsonProperty("sectionType")]
+		public string SectionType { get; set; }
+	}
+}
