e3c826a383c9dbdbf55ca63857ef2c0ee756fb17
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-05-24 07:19:54 +0800

    Bark refactor.

--- GOGGiveawayNotifier/Notifier/Barker.cs
+++ GOGGiveawayNotifier/Notifier/Barker.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Web;
-using System.Text;
 using System.Threading.Tasks;
-using HtmlAgilityPack;
 using Microsoft.Extensions.Logging;
 using GOGGiveawayNotifier.Model;
 using System.Collections.Generic;
+using System.Net.Http;
 
 namespace GOGGiveawayNotifier.Notifier {
 	class Barker : INotifiable {
@@ -21,20 +20,24 @@ namespace GOGGiveawayNotifier.Notifier {
 
 		public async Task SendMessage(NotifyConfig config, List<GiveawayRecord> games) {
 			try {
-				var sb = new StringBuilder();
-				string url = sb.AppendFormat(NotifyFormatStrings.barkUrlWithTitleFormat, config.BarkAddress, config.BarkToken).ToString();
-				var webGet = new HtmlWeb();
+				string url = string.Format(NotifyFormatStrings.barkUrlWithTitleFormat, config.BarkAddress, config.BarkToken);
+				string projectLink = HttpUtility.UrlEncode(NotifyFormatStrings.projectLink);
+
+				var client = new HttpClient();
 
 				foreach (var game in games) {
-					sb.Clear();
-					_logger.LogDebug($"{debugSendMessage} : {game.Name}");
-					await webGet.LoadFromWebAsync(
-						sb.Append(url)
-						.Append(HttpUtility.UrlEncode(game.Name))
-						.Append(HttpUtility.UrlEncode(NotifyFormatStrings.projectLink))
-						.AppendFormat(NotifyFormatStrings.barkUrlArgs, HttpUtility.UrlEncode(game.Url))
-						.ToString()
-					);
+					_logger.LogDebug($"{debugSendMessage} : {game.Title}");
+
+					string args = string.Format(NotifyFormatStrings.barkUrlArgs, HttpUtility.UrlEncode(game.Url));
+					string message = string.Empty;
+
+					if (game.Type == ParseStrings.typeGiveaway)
+						message = $"Giveaway\n\n{game.Title}\n\nEnd Date: {game.EndDate} (CST)";
+					else message = $"Free Game\n\n{game.Title}";
+
+					var resp = await client.GetAsync($"{url}{HttpUtility.UrlEncode(message)}{projectLink}{args}");
+
+					_logger.LogDebug(await resp.Content.ReadAsStringAsync());
 				}
 
 				_logger.LogDebug($"Done: {debugSendMessage}");
